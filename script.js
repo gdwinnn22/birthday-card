f766e82d909ab924bf147b157e6ce118712937ae
gdwinnn22 <gagah.godwin2605@gmail.com>
2026-01-26 19:20:33 +0700

    date-javascript

--- script.js
+++ script.js
@@ -32,6 +32,40 @@ document.addEventListener("click", () => {
   });
 });
 
-if (selected.textContent === "Februari") {
+const month = document.getElementById("month");
+
+function changeDays() {
+
+  selectedList.forEach(selected => {
+
+  const option = selected.nextElementSibling;
+  let daysInMonth;
+
+  option.querySelectorAll("div").forEach(element => {
+    element.addEventListener("click", (e) => {
+      if (selected.textContent === "Februari") {
+        daysInMonth = 28;
+      } else if (selected.textContent === "April" || selected.textContent === "Juni" || selected.textContent === "September" || selected.textContent === "November") {
+        daysInMonth = 30;
+      } else {
+        daysInMonth = 31;
+      }
+    });
+  });
   
+  const daySelect = document.getElementById("day");
+  daySelect.innerHTML =
+    `<div class="options tanggal">`;
+
+  for (let i = 1; i <= daysInMonth; i++) {
+    const day = i.toString().padStart(2, "0");
+    daySelect.innerHTML += `<div data-value="${day}">${day}</div>`;
+  };
+
+  });
+
 };
+
+month.querySelectorAll("div").forEach(el => {
+  el.addEventListener("click", changeDays);
+});
